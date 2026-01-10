dcc02c96243ce044b9e580232cedeb6e797efeea
Willie ( J. )  Conway <hire.willie.conway@gmail.com>
2026-01-09 21:44:57 -0500

    update portfolio

---
+++ SQL - A Practical Introduction for Querying Databases/Labs/Introduction to Relational Databases and Tables/Labs/Create and Load Tables using SQL Scripts/Screenshots/deepseek_sql_20260110_ad4b8c.sql
@@ -0,0 +1,12 @@
+-- Basic table list
+SHOW TABLES;
+
+-- Show tables with type
+SHOW FULL TABLES;
+
+-- Show tables with details
+SHOW TABLE STATUS;
+
+-- Show only tables starting with specific letters
+SHOW TABLES LIKE 'emp%';
+SHOW TABLES LIKE 'd%';
---
+++ SQL - A Practical Introduction for Querying Databases/Labs/Introduction to Relational Databases and Tables/Labs/Create and Load Tables using SQL Scripts/Select_from_Each_Table_Individually.sql
@@ -0,0 +1,16 @@
+USE HR;
+
+-- Employees table
+SELECT * FROM employees;
+
+-- Departments table
+SELECT * FROM departments;
+
+-- Jobs table
+SELECT * FROM jobs;
+
+-- Locations table
+SELECT * FROM locations;
+
+-- Job History table
+SELECT * FROM job_history;
